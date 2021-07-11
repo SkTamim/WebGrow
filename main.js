ae7e666b7ccd4b33171559ea392113238b4810a3
Tamim <sktamim225@gmail.com>
2021-07-11 22:31:15 +0530

    Completed testimonial section with responsiveness

--- main.js
+++ main.js
@@ -163,4 +163,20 @@ $(document).ready(function() {
 });
 
 
+// Swiper JS Slider inisializition for Testimonial Section 
+new Swiper('.testimonial_slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
 
